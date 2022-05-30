9dc0a28ff7371b3b29b11be177d8e6ffd8f16bab
Marionely <iroman_3@mail.ru>
2022-05-30 12:09:24 +0300

    dev commit

--- index.php
+++ index.php
@@ -2,4 +2,34 @@
 
 echo "Hello master";
 
-echo "Hello dev";
+
+//////////////////////////////////////// DEV
+
+
+function sumExample($a, $b){
+    return $a + $b;
+}
+
+function differenceExample($a, $b){
+    return $a - $b;
+}
+
+function multipleExample($a, $b){
+    return $a * $b;
+}
+
+function divisionExample($a, $b){
+    return $a / $b;
+}
+
+$resSum = sumExample(3, 4);
+echo $resSum;
+
+$resDiff = differenceExample(12, 5);
+echo $resDiff;
+
+$resMultiple = multipleExample(4,4);
+echo $resMultiple;
+
+$resDivision = divisionExample(10, 2);
+echo $resDivision;
